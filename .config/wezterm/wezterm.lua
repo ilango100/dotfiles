bdd56c29255bc9a0c63e9b15afcf001fc79ccc73
Ilango Rajagopal <ilangokul@gmail.com>
2023-04-03 15:25:36 +0530

    wezterm: maximize, sway: screenshots to dir

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,27 +1,17 @@
 local wezterm = require("wezterm")
 
-wezterm.on("format-tab-title", function(tab)
-	local pane = tab.active_pane
-	local title = pane.title
-	if pane.domain_name then
-		title = '(' .. pane.domain_name .. ') - ' .. title
-	end
-	return title
-end)
-
-local config = {
-	check_for_updates = false,
-	color_scheme = "Gruvbox dark, medium (base16)",
-	font = wezterm.font("Iosevka Term"),
-	audible_bell = "Disabled",
-
-	keys = {
-		{ mods = "CTRL|SHIFT", key = "D", action = wezterm.action.ShowLauncherArgs { flags = "DOMAINS" } }
-	},
-
-	exit_behavior = "CloseOnCleanExit",
-	clean_exit_codes = { 130 },
+local config = wezterm.config_builder()
+config.check_for_updates = false
+config.color_scheme = "GruvboxDark"
+config.font = wezterm.font("Iosevka Term")
+config.audible_bell = "Disabled"
+config.keys = {
+	{ mods = "CTRL|SHIFT", key = "D", action = wezterm.action.ShowLauncherArgs { flags = "DOMAINS" } },
+	{ mods = "CTRL", key = "UpArrow", action = wezterm.action.ScrollToPrompt(-1) },
+	{ mods = "CTRL", key = "DownArrow", action = wezterm.action.ScrollToPrompt(1) },
 }
+config.exit_behavior = "CloseOnCleanExit"
+config.clean_exit_codes = { 130 }
 
 local wsl_domains = wezterm.default_wsl_domains()
 if next(wsl_domains) then
@@ -40,5 +30,25 @@ if next(ssh_hosts) then
 	end
 	config.ssh_domains = ssh_domains
 end
+
+-- Maximize all displayed windows on startup
+wezterm.on('gui-attached', function(domain)
+	local workspace = wezterm.mux.get_active_workspace()
+	for _, window in ipairs(wezterm.mux.all_windows()) do
+		if window:get_workspace() == workspace then
+			window:gui_window():maximize()
+		end
+	end
+end)
+
+-- Prepend (<domain>) in tab title
+wezterm.on("format-tab-title", function(tab)
+	local pane = tab.active_pane
+	local title = pane.title
+	if pane.domain_name then
+		title = '(' .. pane.domain_name .. ') - ' .. title
+	end
+	return title
+end)
 
 return config
